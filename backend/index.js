7c9b4bd5af80ded080ea4bd74bb54667c3e6d6c5
MintuKumar07 <mintustm14@gmail.com>
2024-12-15 17:32:11 +0530

    cloudinary updated

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,15 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 import path from "path";
+import cloudinary from "cloudinary";
 
 dotenv.config({});
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
+});
 
 const app = express();
 
